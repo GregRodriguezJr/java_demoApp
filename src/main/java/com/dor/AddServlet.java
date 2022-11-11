b1fb58458e231b2c976adad4e56666bddf72d227
Greg <greg.rodriguez@outlook.com>
2022-11-11 14:25:01 -0600

    completed get request to render to client

--- src/main/java/com/dor/AddServlet.java
+++ src/main/java/com/dor/AddServlet.java
@@ -1,12 +1,23 @@
 package com.dor;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class AddServlet extends HttpServlet{
 	
-	public void service(HttpServletRequest req, HttpServletResponse res) {
+	public void service(HttpServletRequest req, HttpServletResponse res) throws IOException {
 		
+		int num1 = Integer.parseInt(req.getParameter("num1"));
+		int num2 = Integer.parseInt(req.getParameter("num2"));
+		
+		int sum = num1 + num2;
+		
+		
+		PrintWriter out = res.getWriter();
+		out.println("The sum of " + num1 + " and " + num2  + " is " + sum);
 	}
 }
