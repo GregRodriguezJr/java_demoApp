c7e3fb7e9e22a1e076d88b69e94a1b42b3ff0358
Greg <greg.rodriguez@outlook.com>
2022-11-11 21:08:55 -0600

    added and tested send redirect

--- src/main/java/com/dor/AddServlet.java
+++ src/main/java/com/dor/AddServlet.java
@@ -17,16 +17,28 @@ public class AddServlet extends HttpServlet {
 		int num2 = Integer.parseInt(req.getParameter("num2"));
 
 		int sum = num1 + num2;
-
-	
-		req.setAttribute("sum", sum);
-	
-		RequestDispatcher rd = req.getRequestDispatcher("sq");
-		rd.forward(req, res);
-
+		
+		/* Prints to browser the sum with AddServlet */
 		/*
 		 * PrintWriter out = res.getWriter(); out.println("The sum of " + num1 + " and "
 		 * + num2 + " is " + sum);
 		 */
+
+		/* Call SqServlet from this AddServlet and pass the sum as attribute */
+	
+		/* req.setAttribute("sum", sum); */
+	
+		/*
+		 * Dispatcher used to call 2nd servlet from 1st servlet that was trigger by
+		 * client/browser using req and res objects
+		 */
+		
+		/*
+		 * RequestDispatcher rd = req.getRequestDispatcher("sq"); rd.forward(req, res);
+		 */
+		
+		res.sendRedirect("sq?sum=" + sum);
+		
+
 	}
 }
--- src/main/java/com/dor/SqServlet.java
+++ src/main/java/com/dor/SqServlet.java
@@ -11,11 +11,20 @@ public class SqServlet extends HttpServlet {
 	
 	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
 		
-		int sum = (int)req.getAttribute("sum");
+		/* Pass the sum from AddServlet and print on the browser using response object */
 		
-		int square = sum * sum;
 		
-		PrintWriter out = res.getWriter();
-		out.println("Square is: " + square);
+		/* int sum = (int)req.getAttribute("sum"); */ 
+		 
+		 int sum = Integer.parseInt(req.getParameter("sum"));
+		 
+		 int square = sum * sum; PrintWriter
+		 
+		 out = res.getWriter(); 
+		 
+		 out.println("Square is: " + square);
+		 
+		
+		System.out.println("sq called");
 	}
 }
