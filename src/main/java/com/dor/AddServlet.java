c0b99b222318759d670a4b958b6e14d6b291ba06
Greg <greg.rodriguez@outlook.com>
2022-11-11 20:33:09 -0600

    added and tested SqServlet

--- src/main/java/com/dor/AddServlet.java
+++ src/main/java/com/dor/AddServlet.java
@@ -3,21 +3,30 @@ package com.dor;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class AddServlet extends HttpServlet{
-	
-	public void service(HttpServletRequest req, HttpServletResponse res) throws IOException {
-		
+public class AddServlet extends HttpServlet {
+
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
+
 		int num1 = Integer.parseInt(req.getParameter("num1"));
 		int num2 = Integer.parseInt(req.getParameter("num2"));
-		
+
 		int sum = num1 + num2;
-		
-		
-		PrintWriter out = res.getWriter();
-		out.println("The sum of " + num1 + " and " + num2  + " is " + sum);
+
+	
+		req.setAttribute("sum", sum);
+	
+		RequestDispatcher rd = req.getRequestDispatcher("sq");
+		rd.forward(req, res);
+
+		/*
+		 * PrintWriter out = res.getWriter(); out.println("The sum of " + num1 + " and "
+		 * + num2 + " is " + sum);
+		 */
 	}
 }
---
+++ src/main/java/com/dor/SqServlet.java
@@ -0,0 +1,21 @@
+package com.dor;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class SqServlet extends HttpServlet {
+	
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+		
+		int sum = (int)req.getAttribute("sum");
+		
+		int square = sum * sum;
+		
+		PrintWriter out = res.getWriter();
+		out.println("Square is: " + square);
+	}
+}
